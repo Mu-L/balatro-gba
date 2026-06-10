2665f5bff5334450d100ef1249cbcf292de7c9ff
Rickey Fehr <ric@rf3.xyz>
2026-06-09 19:30:58 -0700

    Revert "Temporarily remove "new run" and "resume" buttons"
    This reverts commit 55073b5b1d72484dca678f8003f6cb9a26aeea82.

--- source/game/run_setup.c
+++ source/game/run_setup.c
@@ -112,9 +112,8 @@ static StateMachine run_setup_sm = {
  ******************************************************************************/
 
 // clang-format off
-// Uncomment when tab row is re-added
-//static const Rect     RUN_SETUP_RESUME_TAB_DISABLED_SRC                   = {19, 22, 23, 23};
-//static const BG_POINT RUN_SETUP_RESUME_TAB_DISABLED_DEST_POS              = {15, 1};
+static const Rect     RUN_SETUP_RESUME_TAB_DISABLED_SRC                   = {19, 22, 23, 23};
+static const BG_POINT RUN_SETUP_RESUME_TAB_DISABLED_DEST_POS              = {15, 1};
 
 static const BG_POINT RUN_SETUP_FRAME_BG_SE_FILL_SRC_POS                  = {9 , 31};
 
@@ -656,9 +655,8 @@ static void choose_deck_substate_init(void)
     // TODO: add left/right navigation arrows once more decks have been implemented
 
     // Set Tab to "New Run"
-    // Uncomment when tab row is re-added
-    //main_bg_se_copy_rect(RUN_SETUP_RESUME_TAB_DISABLED_SRC, RUN_SETUP_RESUME_TAB_DISABLED_DEST_POS);
-    //tab_set_highlight(RUN_SETUP_TAB_NEW_RUN);
+    main_bg_se_copy_rect(RUN_SETUP_RESUME_TAB_DISABLED_SRC, RUN_SETUP_RESUME_TAB_DISABLED_DEST_POS);
+    tab_set_highlight(RUN_SETUP_TAB_NEW_RUN);
 
     // Set button highlights
     button_set_highlight(&change_deck_button, true);
